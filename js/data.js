b38a5dc0f1605c15b088e357afa9e4f4429cb004
Adediran David <mightydave8@gmail.com>
2024-07-26 21:52:06 +0100

    updated

--- js/data.js
+++ js/data.js
@@ -14,7 +14,6 @@ const servicesData = [
     title: "Backend Development",
     category: "backend",
     description: [
-      '<br>',
       'My backend development expertise ensures robust and scalable solutions, managing server-side operations, database interactions, and API integrations to support complex applications.',
       'With proficiency in languages like <strong>Node.js</strong> and <strong>PHP</strong>, and experience with databases such as <strong>MySQL</strong> and <strong>MongoDB</strong>, I build secure and efficient backend systems.',
       'My focus is on creating reliable architectures that handle high traffic and large data volumes while maintaining performance.',
@@ -25,7 +24,6 @@ const servicesData = [
       title: "Fullstack Development",
       category: "fullstack",
       description: [
-        '<br>',
         'Combining both frontend and backend skills, I offer comprehensive fullstack development services using versatile technology stacks.',
         'These include the <strong>MERN stack (MongoDB, Express.js, React, Node.js)</strong> for JavaScript-based development, and a combination of <strong>PHP</strong> and <strong>MySQL</strong> for robust server-side solutions.',
         'My approach ensures cohesive and fully integrated web applications, leveraging the strengths of these technologies.',
